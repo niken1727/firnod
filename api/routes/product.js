3d6645bf7bb5fc3efeb7421089dfd1e8f153dd63
niken1727 <niken1727@gmail.com>
2020-05-20 18:42:26 +0545

    in process

--- api/routes/product.js
+++ api/routes/product.js
@@ -28,13 +28,69 @@ router.post('/', (req, res, next) => {
  
 });
 
+
+router.get('/', (req, res, next) => {
+    Products.find()
+    .exec()
+    .then(docs => {
+        if(docs.length > 0){
+            res.status(200).json(docs);
+        } else {
+            res.status(404).json({"message": "Data not found"});
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            "error": err
+        });
+    });
+});
+
+router.patch("/:productId", (req, res, next)=>{
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Products.updateMany({_id: id}, {
+        $set: updateOps
+    })
+    .exec()
+    .then(result => {
+        res.status(200).json({"message": "Updated Succesfully"});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({"error": "Cannot update right now"});
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Products.remove({_id: req.params.productId})
+    .exec()
+    .then(result => {
+        res.status(200).json({"message": "Deleted successfully"});
+    })
+    .catch(err => {
+        res.status(500).json({"error": err});
+    });
+});
+
 router.get('/:productId', (req, res, next)=>{
     const id = req.params.productId;
     Products.findById(id).exec()
     .then(doc => {
-        res.status(200).json(doc);
+        if(doc){
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({"message": "No Value found"});
+        }
     })
     .catch(err => {
+        res.status(500).json({
+            error: err
+        });
          console.log(err);
     });
     // if(id == 27){
