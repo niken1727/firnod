5aba10eb9d66e1ce8db83d9a1a55ea3fd1a8138a
niken1727 <niken1727@gmail.com>
2020-05-20 17:48:17 +0545

    changes

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,23 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        name: "Niken Maharjan",
-        age: 20,
-        address: "Purnachandi-20, Lalitpur"
+const Products = require('../models/products');
+
+router.post('/', (req, res, next) => {
+    const products = new Products({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        age: req.body.age
     })
+    products.save().then(result => {
+        console.log(result);
+        res.status(200).json({
+            name: "Niken Maharjan",
+            age: 20,
+            address: "Purnachandi-20, Lalitpur",
+            createdProduct: products
+        });
+    })
+    .catch(err => { 
+        res.status(500).json({
+            error: err
+        });
+        console.log(err);
+    });
+ 
 });
 
 router.get('/:productId', (req, res, next)=>{
     const id = req.params.productId;
-    if(id == 27){
-        res.status(200).json({
-            name: "Niken Maharjan",
-            age: 20,
-            address: "Purnachandi-20, Lalitpur"
-        })
-    }
+    Products.findById(id).exec()
+    .then(doc => {
+        res.status(200).json(doc);
+    })
+    .catch(err => {
+         console.log(err);
+    });
+    // if(id == 27){
+    //     res.status(200).json({
+    //         name: "Niken Maharjan",
+    //         age: 20,
+    //         address: "Purnachandi-20, Lalitpur"
+    //     })
+    // }
 });
 
 
--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 const detailRoute = require('./api/routes/product');
 
+mongoose.connect("mongodb+srv://niken1727:" + process.env.MONGO_ATLAS_PW + "@node-plfws.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Header', "Origin, X-Requested-With, COntent-Type, Accept, Authorization");
+    if(req.method == "OPTIONS"){
+        res.header('Access-Control-Allow_methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+});
 
 app.use('/details', detailRoute);
+
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
